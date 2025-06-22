f01587ec0aca7a07229384fc761c3a1d91e07238
Chanondevs <chanondevs@gmail.com>
2025-06-23 06:21:34 +0700

    update Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Kanit } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 const kanit = Kanit({
   subsets: ['latin'], // หรือ 'thai' ก็ได้ถ้ามี
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="th" className={kanit.variable}>
       <body className="font-kanit">{children}</body>
       <SpeedInsights />
+      <Analytics />
     </html>
   );
 }
